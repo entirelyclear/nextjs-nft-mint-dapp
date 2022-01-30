972c25c3200043f5db65343a7e16627c54069383
entirelyclear <98671834+entirelyclear@users.noreply.github.com>
2022-01-29 21:30:48 -0500

    Update Team.tsx

--- components/Team.tsx
+++ components/Team.tsx
@@ -8,20 +8,20 @@ export default function Team() {
       <h2 className="text-2xl text-gray-100 mb-4">Creator & Developer</h2>
       <Image
         src={Creator}
-        alt="Koji Mochizuki"
+        alt="Artist Twitter"
         width={200}
         height={200}
         className="rounded-full"
       />
       <p className="mt-4">
         <a
-          href="https://twitter.com/kjmczk"
+          href="https://twitter.com/project_twitter"
           rel="noopener noreferrer"
           target="_blank"
           className="text-blue-400"
         >
           <span className="border-2 border-gray-700 hover:border-gray-600 rounded-full px-4 py-2 bg-gray-900">
-            @kjmczk
+            @HoneyBeeHelpers
           </span>
         </a>
       </p>
